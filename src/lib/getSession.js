3d8925f74966fac2bd2ae5539a79c44eb2b85d57
hassaammgl <h455a4mmehtab@gmail.com>
2026-07-15 14:37:13 +0500

    fixed configs

--- src/lib/getSession.js
+++ src/lib/getSession.js
@@ -2,8 +2,10 @@ import { getAuth } from "./auth";
 import { headers } from "next/headers";
 
 export async function getSession() {
+  const headersList = await headers();
+  const plainHeaders = Object.fromEntries(headersList.entries());
   const session = await (await getAuth()).api.getSession({
-    headers: await headers(),
+    headers: plainHeaders,
   });
 
   if (session?.user?.blocked) {
